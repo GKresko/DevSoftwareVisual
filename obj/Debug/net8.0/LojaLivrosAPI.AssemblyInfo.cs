a30f8ff71348b55b20ef8e7c89f7a661b93b5b87
Gustavo Claudino Kresko <gustavock@whbbrasil.com.br>
2024-09-23 13:51:04 -0300

    Alteração .json

--- obj/Debug/net8.0/LojaLivrosAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/LojaLivrosAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LojaLivrosAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a219ff95bad5b754e3c0acbc1b6e8192b6ba2611")]
 [assembly: System.Reflection.AssemblyProductAttribute("LojaLivrosAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LojaLivrosAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
