18345f4c7aa5c31767b91199714f8eb0839f125c
Guilherme Rodrigues Tavares <Guilherme.RTavares@outlook.com>
2024-09-23 21:34:29 -0300

    Criacao base do AutorController

---
+++ Controllers/AutorController.cs
@@ -0,0 +1,15 @@
+using LojaLivrosAPI.Data;
+using LojaLivrosAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LojaLivrosAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AutorController : ControllerBase
+    {
+        
+    }
+}
